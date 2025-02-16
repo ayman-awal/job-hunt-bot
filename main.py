35a1ded668bb1f424150acafa6f8b68afd9b1c82
ayman-awal <ayman.awal36@gmail.com>
2025-02-16 19:52:58 +0600

    Parse job descriptions with BeautifulSoup

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 from dotenv import load_dotenv # type: ignore
 import requests # type: ignore
+from bs4 import BeautifulSoup
+import re
 
 load_dotenv()
 
@@ -9,20 +11,42 @@ cse_id = os.getenv("CSE_ID")
 
 
 query = 'Software Engineer "USA" OR "United States" site:boards.greenhouse.io'
+keywords = ["entry level", "junior", "level I", "software engineer"]
 
-results = []
+jobData = []
 for start in range(1, 101, 10): 
     url = f"https://www.googleapis.com/customsearch/v1?q={query}&key={api_key}&cx={cse_id}&start={start}"
     response = requests.get(url)
     data = response.json()
     
     if 'items' in data:
-        results.extend(data['items'])
+        jobData.extend(data['items'])
     else:
         break
 
-print(len(results))
-for item in results:
-    # print(f"Title: {item['title']}")
-    # print(f"URL: {item['link']}")
-    print(item)
+
+filteredJobs = []
+
+for data in jobData:
+    # print(data['link'])
+    # print(data['title'])
+    # print("\n")
+    try:
+        response = requests.get(data['link'], headers={"User-Agent": "Mozilla/5.0"})
+        response.raise_for_status() 
+        
+        soup = BeautifulSoup(response.text, "html.parser")
+
+        page_text = soup.get_text(separator=" ").lower()
+
+        if any(re.search(rf"\b{k.lower()}\b", page_text) for k in keywords):
+            print("adding job")
+            filteredJobs.append({"title": data['title'], "url": data['link']})
+
+    except Exception as e:
+        print(f"Error processing {url}: {e}")
+
+with open("filtered_jobs.txt", "w") as f:
+    for job in filteredJobs:
+        f.write(job['title'] + "\n" + job['url'] + "\n")
+
