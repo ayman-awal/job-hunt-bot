58d68873a8ddfbb3bc38271e04ead3e7deed8f48
ayman-awal <ayman.awal36@gmail.com>
2025-02-17 11:40:10 +0600

    integrate Sendgrid API to send emails

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import os
 from dotenv import load_dotenv # type: ignore
 import requests # type: ignore
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup # type: ignore
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
 import re
 
 load_dotenv()
 
-api_key = os.getenv("GOOGLE_SEARCH_API_KEY")
+google_search_api_key = os.getenv("GOOGLE_SEARCH_API_KEY")
 cse_id = os.getenv("CSE_ID")
+sendgrid_api_key = os.getenv("SENDGRID_API_KEY")
+from_email = os.getenv("FROM_EMAIL")
+to_email = os.getenv("TO_EMAIL")
 
 
 query = 'Software Engineer "USA" OR "United States" site:boards.greenhouse.io'
@@ -15,9 +21,12 @@ keywords = ["entry level", "junior", "level I", "software engineer"]
 
 jobData = []
 for start in range(1, 101, 10): 
-    url = f"https://www.googleapis.com/customsearch/v1?q={query}&key={api_key}&cx={cse_id}&start={start}"
+    print("here")
+    url = f"https://www.googleapis.com/customsearch/v1?q={query}&key={google_search_api_key}&cx={cse_id}&start={start}"
     response = requests.get(url)
+    print(response)
     data = response.json()
+    print(data)
     
     if 'items' in data:
         jobData.extend(data['items'])
@@ -28,11 +37,8 @@ for start in range(1, 101, 10):
 filteredJobs = []
 
 for data in jobData:
-    # print(data['link'])
-    # print(data['title'])
-    # print("\n")
     try:
-        response = requests.get(data['link'], headers={"User-Agent": "Mozilla/5.0"})
+        response = requests.get(data, headers={"User-Agent": "Mozilla/5.0"})
         response.raise_for_status() 
         
         soup = BeautifulSoup(response.text, "html.parser")
@@ -50,3 +56,24 @@ with open("filtered_jobs.txt", "w") as f:
     for job in filteredJobs:
         f.write(job['title'] + "\n" + job['url'] + "\n")
 
+
+def sendEmail():
+    content = ""
+
+    with open("filtered_jobs.txt", "r") as f:
+        content = f.read() 
+        print(content) 
+
+    message = Mail(
+        from_email=from_email,
+        to_emails=to_email,
+        subject='Trial',
+        html_content=content)
+    try:
+        sg = SendGridAPIClient(sendgrid_api_key)
+        response = sg.send(message)
+        print(response.status_code)
+        print(response.body)
+        print(response.headers)
+    except Exception as e:
+        print(e.message)
